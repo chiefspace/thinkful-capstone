496b4aae50f8026e3b08c2c7376589e904ef0ab8
Ben Altieri <chiefspace@gmail.com>
2017-06-21 05:37:43 +0000

    Created initial endpoints and in memory json data structure for the inventories dictionary of dictionaries

--- app.py
+++ app.py
@@ -7,9 +7,35 @@ from os import environ
 """  Give the app a name  """
 app = Flask(__name__)
 
+inventories = [{
+    'name': 'Laptop Computers',
+    'items': [{'name':'baltieri-lenovo1', 'cost': 1299.99 }]
+}]
+
 """  Tell the app what requests its going to understand  """
-@app.route('/')
-def home():
-    return "Hello, World!"
+# POST /inventory data: (name: )
+@app.route('/inventory', methods=['POST'])
+def create_inventory():
+    pass
+
+# GET /inventory/<string:name>
+@app.route('/inventory/<string:name>')
+def get_inventory(name):
+    pass
+
+# GET /inventory
+@app.route('/inventory')
+def get_inventories():
+    pass
+
+# POST /store/<string:name>/item (name:, cost: )
+@app.route('/inventory/<string:name>/item', methods=['POST'])
+def create_item_in_inventory(name):
+    pass
+
+# GET /inventory/<string:name>/item
+@app.route('/inventory/<string:name>/item')
+def get_item_in_inventory(name):
+    pass
 
 app.run(host=environ['IP'], port=int(environ['PORT']))
