d8c2ad96356dde99b59e6b68409c3393d7c8c466
Ben Altieri <chiefspace@gmail.com>
2017-06-24 03:42:07 +0000

    Instantiated an object of the Flask class called app and also instantiated an Api object called api

--- code/app.py
+++ code/app.py
@@ -1,2 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
+
+app = Flask(__name__)
+
+""" The Api works with Resources and every resource has to be a Class """
+api = Api(app) # instantiate an instance of the Api Class
