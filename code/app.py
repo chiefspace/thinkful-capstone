83172ecf124e01cd448db89f4e50763dbead0bc2
Ben Altieri <chiefspace@gmail.com>
2017-06-25 15:13:42 +0000

    added code for the Item Resource post method.  Will refactor the get method next so that we can test the get and post methods using Postman

--- code/app.py
+++ code/app.py
@@ -15,10 +15,19 @@ items = []
 """ The Item Resource Class will be used to instantiate item objects """
 """ The item object will inherit properties from the Resource Class """
 class Item(Resource):
+    """ get method for retrieving an item Resource object by name and cost """
     def get(self, name):
         return {'item': name}
         
-""" The api object below defines the endpoint for the GET item by name Resource """
+    """ post method for storing an item Resource object by name """
+    """ The cost value is temporarily hard coded to $1.299.00 for now """
+    def post(self, name):
+        item = {'name': name, 'cost': 1299.00}
+        items.append(item)
+        return item
+        
+        
+""" The api object method below defines the endpoint for the GET item by name Resource """
 api.add_resource(Item, '/item/<string:name>')        
 
 """ The app.run method below starts the Flask app and binds it to port 5000 """
