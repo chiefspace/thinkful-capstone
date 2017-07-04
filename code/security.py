0a863eb2432aa490f11bc9c91582d6bc7b749039
Ben Altieri <chiefspace@gmail.com>
2017-07-04 17:12:06 +0000

    Added authenticate function for returning user object by username lookup which also ensures the password entered by the user equals the password stored in the in-memory database. Also added a default value of None which is not possible by just using the square brackets [] to describe the list of user objects.

--- code/security.py
+++ code/security.py
@@ -6,3 +6,22 @@ users = [
 
     }
 ]
+
+username_mapping = { 'ben': {
+        'id': 1,
+        'username': 'ben',
+        'password': '1234'
+    }
+}
+
+userid_mapping = { 1: {
+        'id': 1,
+        'username': 'ben',
+        'password': '1234'
+    }
+}
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and user.password == password:
+        return user
