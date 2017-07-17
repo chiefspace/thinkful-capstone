77a7b37551749014c77d1b940e1cb82d7125ef43
Ben Altieri <chiefspace@gmail.com>
2017-07-17 14:04:48 +0000

    Added class method find_by_username to user.py

--- code/user.py
+++ code/user.py
@@ -5,3 +5,23 @@ class User:
         self.id = _id
         self.username = username
         self.password = password
+    
+    """ Find user by username in the database """
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))  #  Single value tuple
+        
+        row = result.fetchone()
+        if row:
+            user = cls(row[0], row[1], row[2])
+        else:
+            User = None
+            
+        connection.close()  # No need to commit because no data added
+        return user
+        
+        
