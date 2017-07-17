c325d4dd4d4459abde8ad020fe6185068f841eb7
Ben Altieri <chiefspace@gmail.com>
2017-07-17 14:18:09 +0000

    Got rid of in memory user list definition in security.py and also modified the return statement in the identity function to leverage the User.find_by_id method from user.py

--- code/security.py
+++ code/security.py
@@ -1,15 +1,6 @@
 from werkzeug.security import safe_str_cmp
 from user import User
 
-users = [
-    User(1, 'ben', '1234')
-]
-
-""" Implemented a set comprehension to assign key value pairs instead of ... """
-""" ... hard coding the username and userid lists of objects """
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
-
 def authenticate(username, password):
     user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
@@ -17,4 +8,4 @@ def authenticate(username, password):
         
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
