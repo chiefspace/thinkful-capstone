f0f96b10b65711f38faef42a6c1c8d6fae2c009f
Ben Altieri <chiefspace@gmail.com>
2017-07-20 23:08:49 +0000

    Modified the ItemList resource class to leverage SQLAlchemy

--- code/resources/item.py
+++ code/resources/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
@@ -73,14 +72,4 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'assignee': row[1], 'cost': row[2]})
-        connection.close()
-
-        return {'items': items}
+        return {'items': ItemModel.query.all()}
